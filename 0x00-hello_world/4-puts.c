a944bceae3f25f737e673313f3c1fbb1146cb79e
Wassimou <islawassim@gmail.com>
2023-07-13 18:43:20 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <studio.h>
 /**
- *main - main entry point
- *return : always 0 (success)
- */
-int main(void)
-{             
-        puts("\"Programming is like building a multilingal puzzle");
-        return (0);
+ *main -entry poit
+ *return: always 0 (success)
+ /*
+ int main(void)
+ {
+         puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
 }
