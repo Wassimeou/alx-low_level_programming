c6a55b515201da4c17412c8cf9421273a2fe1dd3
Wassimou <islawassim@gmail.com>
2023-07-18 16:30:50 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
-	}
-	-putchar('\n');
+	} else if (n == 0)
 }
