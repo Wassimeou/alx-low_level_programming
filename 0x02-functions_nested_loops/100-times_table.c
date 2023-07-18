d7111cfe87e817f8ab99b79829612a677fd0e25e
Wassimou <islawassim@gmail.com>
2023-07-18 19:47:28 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table  	 	 	 
+ * @n:number of the times table  	 	 	 
  */
 void print_times_table(int n)
 {
@@ -18,7 +18,7 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} 
+				}
 				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
@@ -26,14 +26,15 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} 
+				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');											} 
+					_putchar((k % 10) + '0');
+				}
 				else if (k >= 100)
 				{
 					_putchar(',');
