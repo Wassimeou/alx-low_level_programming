0a83567089cbb4d6c1dd0e2c6317ea7828afea52
Wassimou <islawassim@gmail.com>
2023-08-07 14:59:53 +0100

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,11 +26,11 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; av[i][n]; n++)
-		{
-			str[r] = av[i][n];
-			r++;
-		}
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
 	if (str[r] == '\0')
 	{
 		str[r++] = '\n';
