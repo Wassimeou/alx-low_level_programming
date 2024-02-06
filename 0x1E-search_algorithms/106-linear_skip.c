a8d6b81f4d36ca9c27571c7e24d51d29e33d4aa3
Wassimou <islawassim@gmail.com>
2024-02-06 14:15:05 -0800

    updates

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,44 +1,53 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - searches for a value in a skip list
+ * linear_skip - searches for a value in a sorted skip list of integers
  *
- * @list: input list
- * @value: value to search in
- * Return: index of the number
+ * @list: a pointer to the head of the list to search in
+ * @value: the value to search for
+ *
+ * Return: a pointer to the first node where value is located or NULL
  */
+
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *go;
+	skiplist_t *cur, *prev;
 
-	if (list == NULL)
+	if (!list)
 		return (NULL);
 
-	go = list;
-
-	do {
-		list = go;
-		go = go->express;
-		printf("Value checked at index ");
-		printf("[%d] = [%d]\n", (int)go->index, go->n);
-	} while (go->express && go->n < value);
-
-	if (go->express == NULL)
+	cur = list;
+	while (cur->next && cur->n < value)
 	{
-		list = go;
-		while (go->next)
-			go = go->next;
+		prev = cur;
+		if (cur->express)
+			cur = cur->express;
+		else
+		{
+			while (cur->next)
+				cur = cur->next;
+		}
+		if (cur->next)
+			printf("Value checked at index [%lu] = [%i]\n", cur->index, cur->n);
 	}
 
-	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
+	printf("Value found between indexes [%lu] and [%lu]\n", prev->index,
+	       cur->index);
 
-	while (list != go->next)
+	cur = prev;
+	while (cur && cur->n < value)
 	{
-		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
-		if (list->n == value)
-			return (list);
-		list = list->next;
+		printf("Value checked at index [%lu] = [%i]\n", cur->index, cur->n);
+		if (cur->next)
+			cur = cur->next;
+		else
+			return (NULL);
+	}
+
+	if (cur && cur->n == value)
+	{
+		printf("Value checked at index [%lu] = [%i]\n", cur->index, cur->n);
+		return (cur);
 	}
 
 	return (NULL);
