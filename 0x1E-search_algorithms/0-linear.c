aeb15003552b94ff1eb98f4a5b6a990101909e72
Wassimou <islawassim@gmail.com>
2024-02-07 09:19:01 -0800

    updates

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,28 +1,33 @@
-#include "search_algos.h"
+#include <stdio.h>
 
-/**
- * linear_search - function that searches for a value in an array of
- * integers using the Linear search algorithm.
- * @array: pointer to the first element of the array to search in.
- * @size: number of elements in array.
- * @value: value to search for.
- *
- * If value is not present in array or if array is NULL return -1.
- *
- * Return: first index where value is located.
- */
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i;
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
 
-	if (!array || size <= 0)
-		return (-1);
+    for (size_t i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value) {
+            return i; // Return the index if the value is found
+        }
+    }
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value) /* if (*(array + i) == value) */
-			return (i);
-	}
-	return (-1);
+    // If the value is not found, return -1
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int value = 30;
+
+    int index = linear_search(arr, size, value);
+
+    if (index != -1) {
+        printf("Value %d found at index %d\n", value, index);
+    } else {
+        printf("Value %d not found in the array\n", value);
+    }
+
+    return 0;
 }
